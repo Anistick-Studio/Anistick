19de7ab1235bc919de56481647dcd29bea90f0a1
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-02 00:52:54 -0400

    await

--- movie/parse.js
+++ movie/parse.js
@@ -191,10 +191,8 @@ module.exports = {
 						b = fs.readFileSync(fUtil.getFileIndexForAssets("video-", ".png", id));
 						if (!fs.existsSync(path)) fs.writeFileSync(path, b);
 						const filename = pieces.join(".").slice(0, -3) + "png";
-						asset.load(id, "png").then(buffer => {
-							console.log(buffer);
-							fUtil.addToZip(zip, filename, buffer);
-						});
+						const buffer = await asset.load(id, "png");
+						fUtil.addToZip(zip, filename, buffer);
 					}
 				} catch (e) {
 					console.error(`WARNING: Couldn't find asset ${id}:`, e);
