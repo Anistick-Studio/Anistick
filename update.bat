7e1e81e213f91ce46eb368be38e14336e065cc3b
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-11 00:41:54 -0500

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 call env.bat
 echo Updating Anistick...
-if %ENV%==dev ( git pull || git stash && git pull ) else ( if exist .git ( git pull ) else ( echo Update Failed Because Git was not found. && pause && cls && goto install ) )
+if %ENV%==dev ( git pull || git stash && git pull ) else ( if exist .git ( git pull ) else ( echo Update Failed Because Git was not found. && pause && exit ) )
 echo Update Complete
 pause & exit
